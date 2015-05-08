2df221171a69e1c088381679f086e142109825d3
Mathieu Decoene <hello@mathieudecoene.be>
2015-05-08 10:29:46 +0100

    Updated the UI manifest

--- modules/ui/manifests/init.pp
+++ modules/ui/manifests/init.pp
@@ -10,5 +10,11 @@ class ui (
     class { "::ui::install::${install_method}":
         install_config  => $install_config,
         ui_root_dir     => $ui_root_dir
+    } ->
+
+    # Install the git submodules
+    exec { 'install_git_submodules':
+        cwd        => $ui_root_dir,
+        command    => 'git submodule update --init --recursive',
     }
 }
