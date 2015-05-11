0c7b2f118e7d4588de697b073a774c74a8718ccc
Bert Pareyn <pareyn.bert@gmail.com>
2015-05-11 12:45:58 +0100

    Add configuration to enable/disable production builds, fix variable name to include '@'

--- modules/ui/manifests/init.pp
+++ modules/ui/manifests/init.pp
@@ -7,14 +7,27 @@ class ui (
         'revision' => 'master'
     }) {
 
+    # Create a command for generating the production build
+    $production_build_cmd = hiera('do_production_build') ? {
+        'true'  => 'npm install -d && node ./node_modules/.bin/grunt',
+        'false' => 'true',
+        default => 'true',
+    }
+
     class { "::ui::install::${install_method}":
         install_config  => $install_config,
         ui_root_dir     => $ui_root_dir
     } ->
 
-    # Install the git submodules
+    # Install the git submodules and, if enabled, run a production build
     exec { 'install_git_submodules':
         cwd        => $ui_root_dir,
-        command    => 'git submodule update --init --recursive',
+        command    => 'git submodule update --init --recursive'
+    } ->
+
+    # Do a production build, if it's enabled
+    exec { 'do_production_build':
+        cwd        => $ui_root_dir,
+        command    => $production_build_cmd
     }
 }
